f45082fe98ba2edb2d527134f4bb14f53b7d88b5
Mansirupareliya <mansirupareliya09@gmail.com>
2026-07-12 11:47:14 +0530

    Implemnet a bout us page

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./Component/Sidebar";
-import  Home from "./Pages/Home"
+import Home from "./Pages/Home"
 import Products from "./product";
 import Store from "./Store";
 import ProductDetail from "./ProductDetail";
 import StoreLayout from "./StoreLayout";
 import CartLayout from "./CartLayout";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./Context/CartContext";
 import Cart from "./Pages/Cart";
 import About from "./Pages/About";
 
@@ -16,24 +16,24 @@ function App() {
 
   return (
     <CartProvider>
-    <div className="min-h-screen bg-[var(--bg)] text-[var(--text)]">
-      {!isPublicRoute && <Sidebar />}
+      <div className="min-h-screen bg-[var(--bg)] text-[var(--text)]">
+        {!isPublicRoute && <Sidebar />}
 
-      <main className={isPublicRoute ? "w-full" : "ml-72 flex-1 px-4 py-6 md:px-8"}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/store" element={<StoreLayout />}>
-            <Route index element={<Store />} />
-            <Route path="about" element={<About />} />
-            <Route path=":productId" element={<ProductDetail />} />
-          </Route>
-          <Route path="/cart" element={<CartLayout />}>
-            <Route index element={<Cart />} />
-          </Route>
-        </Routes>
-      </main>
-    </div>
+        <main className={isPublicRoute ? "w-full" : "ml-72 flex-1 px-4 py-6 md:px-8"}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/store" element={<StoreLayout />}>
+              <Route index element={<Store />} />
+              <Route path="about" element={<About />} />
+              <Route path=":productId" element={<ProductDetail />} />
+            </Route>
+            <Route path="/cart" element={<CartLayout />}>
+              <Route index element={<Cart />} />
+            </Route>
+          </Routes>
+        </main>
+      </div>
     </CartProvider>
   );
 }
